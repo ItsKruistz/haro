fb3bcefaa0526cae5d4deed61b2f744edd7e49ec
NotteShock <lekruistz@gmail.com>
2024-07-06 11:24:40 +0000

    update

--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -15,7 +15,7 @@ const buttonVariants = tv({
 			link: "text-primary underline-offset-4 hover:underline",
 		},
 		size: {
-			default: "h-9 px-3.5 py-1.5 text-[15px]",
+			default: "h-9 rounded-lg px-3 py-1.5 text-[15px]",
 			sm: "h-9 rounded-md px-3",
 			lg: "h-11 rounded-md px-8",
 			icon: "h-10 w-10",

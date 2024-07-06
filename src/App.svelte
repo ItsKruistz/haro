db43f1cd0a5a98bd68c2ae4593e8ff478e1ed2a0
NotteShock <lekruistz@gmail.com>
2024-07-06 02:27:09 +0000

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,7 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+</script>
+
 <main>
-  <section class="body-font">
-    <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-      <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-left text-left">
-        <h1 class="text-4xl mb-4 font-bold">Hello, I'm Kruistz!</h1>
-        <p class="mb-8 text-lg leading-normal">An hobbyist coder who likes to play gacha games. I also like to take photos too!</p>
-      <div class="flex">
-        <button
-        class="select-none rounded-lg bg-gray-900 py-3 px-6 text-center align-middle
-        font-sans text-xs font-bold uppercase text-white
-        shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-        type="button"
-      >
-        filled
-      </button>
-      <button
-        class="select-none rounded-lg border border-gray-900 py-3 px-6 text-center align-middle font-sans text-[15px] font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-        type="button"
-      >
-        outlined
-      </button>
-      <button
-        class="px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-        type="button"
-      >
-        text
-      </button>
-      </div>
-    </div>
-    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-      <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
-    </div>
-  </div>
-  </section>
+  <Button>Test</Button>
 </main>

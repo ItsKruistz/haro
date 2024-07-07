92d9bca49a1d0bfc8da12b30110f64a9c21ff5d2
ItsKruistz <lekruistz@gmail.com>
2024-07-07 14:32:43 +0700

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { Button } from "$lib/components/ui/button";
+  import { Checkbox } from "$lib/components/ui/checkbox";
 </script>
 
 <Button>Button</Button>
 <Button variant="outline">Button</Button>
+<Checkbox />

06f4d2e2ab20dc4e7be4bb2c454d944b1f8f7b4b
ItsKruistz <lekruistz@gmail.com>
2024-07-07 18:08:13 +0700

    up

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,20 @@
   import { Checkbox } from "$lib/components/ui/checkbox";
 </script>
 
-<Button>Button</Button>
-<Button variant="outline">Button</Button>
-<Checkbox />
+<main>
+  <section>
+    <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+      <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-left text-left">
+        <h1 class="text-4xl mb-4 font-bold">Hello, I'm Kruistz!</h1>
+        <p class="mb-8 leading-normal text-lg">An hobbyist coder who likes to play gacha games. I also like to take photos too!</p>
+      <div class="flex justify-center">
+        <Button>Button</Button>
+        <Button variant="outline">Button</Button>
+      </div>
+    </div>
+    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
+    </div>
+  </div>
+  </section>
+</main>

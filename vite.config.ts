2f8d7fe38a660657c768a3bea0e6e98e685d3df5
ItsKruistz <lekruistz@gmail.com>
2024-07-13 20:35:28 +0700

    g

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'path';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import viteCompression from 'vite-plugin-compression'; // Import the plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte()],
-    resolve: {
-        alias: {
-            $lib: path.resolve('./src/lib'),
-        },
+  plugins: [
+    svelte(),
+    viteCompression({ // Use the plugin here
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      threshold: 10240,
+      verbose: true,
+      deleteOriginFile: false,
+    }),
+  ],
+  optimizeDeps: {
+    force: true,
+  },
+  resolve: {
+    alias: {
+      $lib: path.resolve('./src/lib'),
     },
-})
+  },
+});

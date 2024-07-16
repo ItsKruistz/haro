aadfb6b8a3d03863eb3c6de9f75a81593781ff0e
ItsKruistz <lekruistz@gmail.com>
2024-07-16 09:04:27 +0700

    g

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { Button } from "$lib/components/ui/button";
   import { Checkbox } from "$lib/components/ui/checkbox";
+  import * as Sheet from "$lib/components/ui/sheet/index.js";
   import { onMount } from 'svelte';
 
   // Define types for state variables
